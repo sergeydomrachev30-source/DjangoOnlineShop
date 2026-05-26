c12f240956021def5d8821db54c1983447af169e
Sergey <new_day_has_come@mail.ru>
2026-05-26 15:33:28 +0300

    feat: connect postgresql, add models, fixtures, custom command and homework screenshots

---
+++ catalog/management/commands/fill_db.py
@@ -0,0 +1,20 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+from catalog.models import Product, Category
+
+
+class Command(BaseCommand):
+    help = "Очищает базу данных и загружает тестовые данные из фикстур"
+
+    def handle(self, *args, **options):
+        # 1. Сначала удаляем старые данные
+        self.stdout.write("Очистка базы данных...")
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
+        # 2. Загружаем данные из наших фикстур
+        self.stdout.write("Загрузка данных из фикстур...")
+        call_command("loaddata", "category_data.json")
+        call_command("loaddata", "product_data.json")
+
+        self.stdout.write(self.style.SUCCESS("База данных успешно перезаполнена!"))
--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,30 @@
+from catalog.models import Product, Contacts
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'catalog/home.html')
+    last_five = Product.objects.all().order_by("-created_at")[:5]
+    print("--- Последние 5 товаров на главной ---")
+    for product in last_five:
+        print(f"Товар: {product.name}, Цена: {product.price}")
+
+    return render(request, "catalog/home.html")
+
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('username')
-        message = request.POST.get('message')
+    # Получаем заполненные контакты из базы данных (самую первую запись)
+    contact_info = Contacts.objects.first()
+    if request.method == "POST":
+        name = request.POST.get("username")
+        message = request.POST.get("message")
 
         # Для проверки вывод в терминал PyCharm
         print(f"Новое сообщение от {name}: {message}")
 
         # Передаем в шаблон флаг успеха и имя пользователя
-        context = {
-            'success': True,
-            'user_name': name
-        }
-        return render(request, 'catalog/contacts.html', context)
-    return render(request, 'catalog/contacts.html')
+        context = {"success": True, "user_name": name}
+        return render(request, "catalog/contacts.html", context)
+
+    # Передаем контакты в обычный шаблон при открытии страницы
+    context = {"contact_info": contact_info}
+    return render(request, "catalog/contacts.html", context=context)
